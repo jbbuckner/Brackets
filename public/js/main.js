a92fc1236840d67d980c27716a2377d454572dfb
JB Buckner <jb@bamads.com>
2014-04-01 17:38:52 -0400

    updating selection process

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,22 @@
 $(function(){
 	console.log('ready');
-	$('.button').click(function(e){
-		e.preventDefault();
-		console.log("hit " + $('.button').className);
+	var placeIndex = ['1-1', '1-2', '2-1', '2-2', '3-1', '3-2'];
+	$('.button').click(function(){
+		var hitClass = $(this).attr('class').split(/\s+/);
+		console.log("hit " + hitClass);
+		$.each( hitClass, function(index, item){
+			console.log('index '+index);
+			for (i=0; i<placeIndex.length; i++){
+				if (item === placeIndex[i]){
+					var value = $('.'+item).text();
+						
+				}
+			}
+			// if (item === '1-1') {
+			// 	var value = $('.1-1').text();
+			// 	console.log('1-1 '+ value);
+			// 	$('.3-1').text(value);
+			// }
+		});
 	});
 });
