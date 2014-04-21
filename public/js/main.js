c96a2e049c6657c2a3cfa7729dfa8a29f3149b51
JB Buckner <jb@bamads.com>
2014-04-21 17:37:48 -0400

    Setting up dynamic number placement

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,21 @@
 $(function(){
 	console.log('ready');
-	var placeIndex = ['1-1', '1-2', '2-1', '2-2', '3-1', '3-2'];
+	var placeIndex = ['1-1', '1-2', '1-3', '1-4', '2-1', '2-2'];
 	$('.button').click(function(){
 		var hitClass = $(this).attr('class').split(/\s+/);
 		console.log("hit " + hitClass);
 		$.each( hitClass, function(index, item){
-			console.log('index '+index);
 			for (i=0; i<placeIndex.length; i++){
 				if (item === placeIndex[i]){
 					var value = $('.'+item).text();
-						
+					console.log(item);
+					var str = item.substr(0,item.indexOf('-'));//find '-' grab everything before and including
+					var place = item.substr(item.indexOf('-')+1, item.length);
+					console.log(place);
+					str++;
+					place = Math.round(place/2);
+					console.log(place);
+					$("."+str+'-'+place).append(value);
 				}
 			}
 			// if (item === '1-1') {
