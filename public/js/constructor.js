df907bcc254bca12c08742f93e05edf06a413b9b
JB Buckner <jb@bamads.com>
2014-07-14 15:01:35 -0400

    clean up

--- public/js/constructor.js
+++ public/js/constructor.js
@@ -1,10 +1,10 @@
 function createBracketMatchups(seeds){
+	//loops through and creates the array 
 	//winners box
 	var ColumnNumber=0;
 	
 	
 	totalColumns = getTotalColumns(seeds);
-	//console.log('totalColumns ', ColumnNumber);
 	for (var j=0; j<totalColumns; j++){
 		var seedsInColumn=Math.pow(2,ColumnNumber);
 		roundArray[ColumnNumber]={};
@@ -28,45 +28,33 @@ function createBracketMatchups(seeds){
 				for(var i=0; i<roundArray[ColumnNumber-1].matchesArray.length; i++){
 
 					roundArray[ColumnNumber].matchesArray[currentRound]={};
-					
-					//var matchesPerRound = seedsInColumn/2;
-						roundArray[ColumnNumber].matchesArray[currentRound].team1 = roundArray[ColumnNumber-1].matchesArray[i].team1;
-						roundArray[ColumnNumber].matchesArray[currentRound].team2 = teams[seedsInColumn-roundArray[ColumnNumber].matchesArray[currentRound].team1.seed];
-						currentRound++
-						roundArray[ColumnNumber].matchesArray[currentRound]={};
+	
+					roundArray[ColumnNumber].matchesArray[currentRound].team1 = roundArray[ColumnNumber-1].matchesArray[i].team1;
+					roundArray[ColumnNumber].matchesArray[currentRound].team2 = teams[seedsInColumn-roundArray[ColumnNumber].matchesArray[currentRound].team1.seed];
+					currentRound++
+					roundArray[ColumnNumber].matchesArray[currentRound]={};
 
-						roundArray[ColumnNumber].matchesArray[currentRound].team1 = roundArray[ColumnNumber-1].matchesArray[i].team2;
-						//console.log('seedsPerRound', seedsInColumn);
-						//console.log('math for seed', seedsInColumn-roundArray[ColumnNumber].matchesArray[currentRound].team1.seed)
-						roundArray[ColumnNumber].matchesArray[currentRound].team2 = teams[seedsInColumn-roundArray[ColumnNumber].matchesArray[currentRound].team1.seed]
-						currentRound++
+					roundArray[ColumnNumber].matchesArray[currentRound].team1 = roundArray[ColumnNumber-1].matchesArray[i].team2;
+					roundArray[ColumnNumber].matchesArray[currentRound].team2 = teams[seedsInColumn-roundArray[ColumnNumber].matchesArray[currentRound].team1.seed]
+					currentRound++
 				}
-				//clearMatches(ColumnNumber, roundArray[ColumnNumber-1].matchesArray.length);
-				// clear previous column array
-				// for(var clearMatch=0; clearMatch<roundArray[ColumnNumber-1].matchesArray.length; clearMatch++){
-				// 	roundArray[ColumnNumber-1].matchesArray[clearMatch].team1 = null;
-				// 	roundArray[ColumnNumber-1].matchesArray[clearMatch].team2 = null;
-				// }
 			break;
 		}
-		buildBracket(ColumnNumber, seedsInColumn);
+		displayBracket(ColumnNumber, seedsInColumn);
 		//clearMatches(ColumnNumber, roundArray[ColumnNumber-1].matchesArray.length);
 		ColumnNumber++;
-		
-
 	}
-	
 }
 
-function buildBracket(ColumnNumber, seedsNow){
-var columnX = 'column'+ColumnNumber;
+function displayBracket(ColumnNumber, seedsNow){
+	//Loops through the array and displays the bracket
+	var columnX = 'column'+ColumnNumber;
 	$('#full-bracket .left').append('<div class="column '+columnX+'"></div>');
-	console.log('seedsNow', seedsNow);
+	//console.log('seedsNow', seedsNow);
 	for(var i=0; i<seedsNow/2; i++){
 		if(ColumnNumber==0){
 			$('#full-bracket .left .'+(columnX)).append('<div class="row">'+roundArray[ColumnNumber].matchesArray[i].team1.seed+'</div>');	
 		}else{
-			//console.log("this seed", roundArray[ColumnNumber].matchesArray[i]);
 			console.log("this seed", roundArray[ColumnNumber].matchesArray[i].team1.seed);
 			$('#full-bracket .left .'+(columnX)).append('<div class="row">'+roundArray[ColumnNumber].matchesArray[i].team1.seed+'</div>');
 			$('#full-bracket .left .'+(columnX)).append('<div class="row">'+roundArray[ColumnNumber].matchesArray[i].team2.seed+'</div>');
@@ -75,6 +63,7 @@ var columnX = 'column'+ColumnNumber;
 }
 function clearMatches(ColumnNumber, arrayLength){
 	// clear previous column array
+	// Starts at the largest column than loops through each column and removes teams generated.
 	console.log("clearMatches", ColumnNumber+" "+arrayLength);
 	for(var clearMatch=0; clearMatch<arrayLength; clearMatch++){
 		roundArray[ColumnNumber-1].matchesArray[clearMatch].team1 = null;
