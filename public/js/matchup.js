f7212a3a0e0e0bb709312039301926b56d61ae43
JB Buckner <jb@bamads.com>
2014-08-06 17:01:36 -0400

    matchups created in bracket

--- public/js/matchup.js
+++ public/js/matchup.js
@@ -7,12 +7,40 @@ var Matchup = {
 		//need round?
 		//columns = ((Math.log(Bracket.numberofTeams)/Math.log(2))+1);
 		for (var i=0; i<totalRounds; i++){
-			var matchup = Matchup.BuildMatchup({
-				round : i
-				//highSeed : Team.TeamObject(),
-				//lowSeed : Team.TeamObject()
-			});
-			matchups.push(matchup);
+			var seedsInColumn = Math.pow(2,i);
+			switch (seedsInColumn){
+				case 1:
+					var matchup = Matchup.BuildMatchup({
+						round : i+1,
+						winner: null,
+						lowSeed : null,
+						competition: false
+					});
+					matchups.push(matchup);
+				break;
+				case 2:
+					var matchup = Matchup.BuildMatchup({
+						round : i+1
+						//highSeed : Team.TeamObject(),
+						//lowSeed : Team.TeamObject()
+					});
+					matchups.push(matchup);
+				break;
+				default:
+				console.log(seedsInColumn);
+					for (var j=0; j<seedsInColumn/2; j++){
+						console.log(j);
+						var matchup = Matchup.BuildMatchup({
+						round : i+1,
+						highSeed : j+1
+						//lowSeed : Team.TeamObject()
+						});
+						matchups.push(matchup);
+					};
+					
+				break;
+			}
+			
 		}
 		return matchups;
 	},
@@ -21,12 +49,12 @@ var Matchup = {
 			this.round = matchupProperties.round;
 			this.highSeed = matchupProperties.highSeed;
 			this.lowSeed = matchupProperties.lowSeed;
-			//this.winner = null;
+			this.winner = null;
 			// this.team1Wins = 0;
 			// this.team2Wins = 0;
 			// this.team1Score = 0;
 			// this.team2Score = 0;
-			this.competition = true;
+			this.competition = matchupProperties.competition;
 		};
 		MatchupObject.prototype.sortMatches = function(){
 
